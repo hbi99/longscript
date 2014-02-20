18c608717792711ebcbbf396ef5941f218271403
Hakan Bilgin <hbi99@hotmail.com>
2014-02-20 20:58:19 +0100

    preview

--- res/js/utils/filesystem.js
+++ res/js/utils/filesystem.js
@@ -3,6 +3,12 @@ sys.fs = {
 	init: function() {
 		this.fs = require('fs');
 		this.path = require('path');
+
+		console.log(1);
+
+		this.load({path: 'res/xml/ledger.xml'}, function() {
+			console.log( arguments );
+		});
 	},
 	dispose: function() {
 		
@@ -13,14 +19,15 @@ sys.fs = {
 	load: function(file, callback) {
 		var fs = this;
 		this.fs.readFile(file.path, 'utf8', function(err, data) {
-			if (err) fs.handleError( err );
+			if (err) return fs.handleError( err );
+			file.text = data;
 			callback(file);
 		});
 	},
 	save: function(file, callback) {
 		var fs = this;
 		this.fs.writeFile(file.path, file.text, 'utf8', function(err) {
-			if (err) fs.handleError( err );
+			if (err) return fs.handleError( err );
 			callback(file);
 		});
 	}
